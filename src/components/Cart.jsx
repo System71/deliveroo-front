370910d532dfb35ebeb73c77c2986b960fcf414b
Pierre-Antoine RIGAUDIER <par@rigaudier.fr>
2024-01-19 10:52:57 +0100

    19-01-2024 10h52

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,4 +1,17 @@
-const Cart = ({ data, cart, setCart }) => {
+import { useState, useEffect } from "react";
+
+const Cart = ({ cart, setCart }) => {
+  const [subtotalCart, setSubtotalCart] = useState(0);
+  const [deliveryCost, setDeliveryCost] = useState(2.5);
+
+  //Quand le panier est mis à jour on met à jour le state du sous-total panier
+  useEffect(() => {
+    const subTotal = cart.reduce((acc, meal) => {
+      return acc + meal.quantity * meal.price;
+    }, 0);
+    setSubtotalCart(subTotal);
+  }, [cart]);
+
   const addQuantity = (index) => {
     const copyCart = [...cart];
     copyCart[index].quantity++;
@@ -35,6 +48,19 @@ const Cart = ({ data, cart, setCart }) => {
               </div>
             );
           })}
+          <div className="sum">
+            <div className="subtotal">
+              <span>Sous-total</span>
+              <span>{subtotalCart} €</span>
+            </div>
+            <div className="delivery">
+              <span>Frais de livraison</span>
+              <span>{deliveryCost} €</span>
+            </div>
+            <div className="total">
+              <span>Total</span> <span>{subtotalCart + deliveryCost} €</span>
+            </div>
+          </div>
         </div>
       ) : (
         <div className="empty-cart">
--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -21,45 +21,46 @@ const Menu = ({ data, cart, setCart }) => {
   };
 
   return data.categories.map((cat, index) => {
-    // cat.meals.length!==0 &&
-
     return (
-      //Que mettre en key à part index? name?
-      <section key={index}>
-        <h2>{cat.name}</h2>
-        <div className="meals">
-          {cat.meals.map((meal) => {
-            return (
-              <div
-                key={meal.id}
-                className="menu-item"
-                onClick={() => {
-                  addToCart(meal);
-                }}
-              >
-                <div className="menu-item-text">
-                  <h3>{meal.title}</h3>
-                  <p>{meal.description}</p>
-                  <div className="item-infos">
-                    <span>{meal.price} €</span>
-                    {meal.popular && (
-                      <span id="popular">
-                        <FontAwesomeIcon icon="star" />
-                        <aside>Populaire</aside>
-                      </span>
-                    )}
+      //On vérifie qu'il a des éléments dans la catégorie pour ne pas afficher que les titres
+      cat.meals.length !== 0 && (
+        //Que mettre en key à part index? name?
+        <section key={index}>
+          <h2>{cat.name}</h2>
+          <div className="meals">
+            {cat.meals.map((meal) => {
+              return (
+                <div
+                  key={meal.id}
+                  className="menu-item"
+                  onClick={() => {
+                    addToCart(meal);
+                  }}
+                >
+                  <div className="menu-item-text">
+                    <h3>{meal.title}</h3>
+                    <p>{meal.description}</p>
+                    <div className="item-infos">
+                      <span>{meal.price} €</span>
+                      {meal.popular && (
+                        <span id="popular">
+                          <FontAwesomeIcon icon="star" />
+                          <aside>Populaire</aside>
+                        </span>
+                      )}
+                    </div>
                   </div>
+                  {meal.picture && (
+                    <div className="menu-item-picture">
+                      <img src={meal.picture} alt="Meal picture" />
+                    </div>
+                  )}
                 </div>
-                {meal.picture && (
-                  <div className="menu-item-picture">
-                    <img src={meal.picture} alt="Meal picture" />
-                  </div>
-                )}
-              </div>
-            );
-          })}
-        </div>
-      </section>
+              );
+            })}
+          </div>
+        </section>
+      )
     );
   });
 };
